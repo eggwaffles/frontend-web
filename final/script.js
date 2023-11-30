8b42b8867f26bcebc659c2f9e8491b051ab016a0
eggwaffles <125239802+eggwaffles@users.noreply.github.com>
2023-11-30 01:01:00 -0500

    update

--- final/script.js
+++ final/script.js
@@ -1,54 +1,42 @@
-let url = "https://api.openweathermap.org/data/2.5/weather?";
-let units = "&units=imperial&q=";
-let city = "Shanghai";
-let apikey = "&appid=a7d4aea0571a0f505398c00a7690b7a4";
-let bgCol = 0;
-let displayText = "Visualize temperature from Open Weather data";
+function getWeather() {
+  let temperature = document.getElementById("temperature");
+  let description = document.getElementById("description");
+  let location = document.getElementById("location");
 
-document.addEventListener("DOMContentLoaded", function () {
-  getWeatherData();
-});
+  let api = "https://api.openweathermap.org/data/2.5/weather?";
+  let apiKey = "&appid=84c7e330020cc82d67165fb90c63fd43";
 
-function getWeatherData() {
-  let request = url + units + city + apikey;
-  
-  fetch(request)
-    .then(response => response.json())
-    .then(dataLoaded)
-    .catch(error => console.error('Error fetching data:', error));
+  console.log("Initiating weather data retrieval...");
+
+  location.innerHTML = "Locating...";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    console.log("Geolocation success: ", latitude, longitude);
+
+    let url = api + "lat=" + latitude + "&lon=" + longitude + apiKey + "&units=imperial";
+
+    fetch(url)
+      .then(response => {
+        console.log("Received response from API");
+        return response.json();
+      })
+      .then(data => {
+        console.log("Data: ", data);
+        let temp = data.main.temp;
+        temperature.innerHTML = temp + "° F";
+        location.innerHTML = data.name;
+        description.innerHTML = data.weather[0].main;
+      })
+  }
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
 }
 
-function dataLoaded(data) {
-    let temperature = data.main.temp;
-    let feelsLike = data.main.feels_like;
-    let tempMin = data.main.temp_min;
-    let tempMax = data.main.temp_max;
-    let pressure = data.main.pressure;
-    let humidity = data.main.humidity;
-    let windSpeed = data.wind.speed;
-    let weatherDescription = data.weather[0].description;
-  
-    displayText = `
-      Temperature: ${temperature} °F
-      Feels Like: ${feelsLike} °F
-      Min Temperature: ${tempMin} °F
-      Max Temperature: ${tempMax} °F
-      Pressure: ${pressure} hPa
-      Humidity: ${humidity}%
-      Wind Speed: ${windSpeed} m/s
-      Weather Description: ${weatherDescription}
-    `;
-
-    updateDOM();
-  }
-  
-
-function updateDOM() {
-  let textElement = document.getElementById("temperature-info");
-  if (!textElement) {
-    textElement = document.createElement("p");
-    textElement.id = "temperature-info";
-    document.body.appendChild(textElement);
-  }
-  textElement.textContent = displayText;
-}
+getWeather();
